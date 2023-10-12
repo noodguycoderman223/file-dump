1e1c4af19608c13c58502ddedbfcb29cb7167fe4
AkaTech <91149805+noodguycoderman223@users.noreply.github.com>
2023-10-12 18:32:08 -0400

    Update extra-text.js

--- extra-text.js
+++ extra-text.js
@@ -1,1 +1,30 @@
+class split {
+  getInfo() {
+    return {
+      id: 'split',
+      name: 'Split',
+      blocks: [
+        {
+          opcode: 'split',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'Split string [ONE] by rule [TWO]',
+          arguments: {
+            ONE: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'Hello!World'
+            },
+            TWO: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: '!'
+            }
+          }
+        }
+      ]
+    };
+  }
 
+  strictlyEquals(args) {
+    return args.ONE.split(args.TWO)
+  }
+}
+Scratch.extensions.register(new split());
